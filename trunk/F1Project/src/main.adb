5d29b09488c0f17487dbce4b1daa9019a560edfd
mick89.mac@gmail.com <mick89.mac@gmail.com@20a5eb55-9242-b5b8-544f-9d6c2917c2bf>
2013-06-03 15:11:01 +0000

    modificato main

--- trunk/F1Project/src/main.adb
+++ trunk/F1Project/src/main.adb
@@ -8,7 +8,8 @@ use Circuit;
 
 procedure Main is
    ref_array : array(1 .. 3) of Referee_Access;
-   test_status : Car_Status_Access;
+   car_array : array(1 .. Circuit.car_number) of Car_Access;
+   --test_status : Car_Status_Access;
    test_speed : Positive := 1;
    test_towait : Positive := 1;
    test_next : Referee_Access := null;
@@ -21,6 +22,8 @@ procedure Main is
    Period    : constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds (1000);
 
 begin
+
+   --Creo circuito di prova
    ref_array(3) := new Referee(3,null,null);
    ref_array(3).setSegment(null);
    ref_array(2) := new Referee(2,null,ref_array(3));
@@ -31,16 +34,20 @@ begin
    ref_array(3).setNext(ref_array(1)); -- chiude il ciclo
 
    --   ref_array(1).enterSegment(1,1, test_speed, 1, test_towait, test_next);
-   test_status := new Car_Status(1,1, 200, 30);
+   --test_status := new Car_Status(1,1, 200, 30);
 
-   test_car := new Car(1,ref_array(1),test_status);
-   test_car2 := new Car(2,ref_array(1),test_status);
-   test_car3 := new Car(3,ref_array(1),test_status);
+   --test_car := new Car(1,ref_array(1),new Car_Status(1,1, 200, 30));
+   --test_car2 := new Car(2,ref_array(1),test_status);
+   --test_car3 := new Car(3,ref_array(1),test_status);
+
+   -- Creo le macchine
+   For_Loop :
+   for i in Integer range 1 .. Circuit.car_number loop
+      car_array(i) := new Car(i,ref_array(1),new Car_Status(i,i, 200, 30));
+   end loop For_Loop;
 
 
-
-
-   Ada.Text_IO.Put_Line ("--> " & Positive'image(test_next.id));
+  -- Ada.Text_IO.Put_Line ("--> " & Positive'image(test_next.id));
 
    Ada.Text_IO.Put_Line ("Hello World!");
    --delay until Poll_Time + Period;
