fcc39269e3e9fc10c0991e36ff83d4d806cd61c1
mick89.mac@gmail.com <mick89.mac@gmail.com@20a5eb55-9242-b5b8-544f-9d6c2917c2bf>
2013-07-03 09:48:39 +0000

    agbgnkjigfdknjlfs

---
+++ F1Project/src/custom_types.ads
@@ -0,0 +1,17 @@
+package custom_types is
+
+   -----------------------------------------------------------------------
+   --------------------------- SEGMENT -----------------------------------
+   -----------------------------------------------------------------------
+
+   type Segment (id : Positive;
+                 length : Positive;
+                 multiplicity : Positive;
+                 difficulty : Positive;
+                 isBoxEntrance : Boolean) is record
+      test : Positive;
+   end record;
+
+   type Segment_Access is access Segment;
+
+end custom_types;
---
+++ F1Project/src/parser.ads
@@ -0,0 +1,5 @@
+package parser is
+
+   function readCircuit (Filename : in String) return Segment_Access;
+
+end parser;
