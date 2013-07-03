99f2592f4eec062043b28837546a28d1308ad566
infernet2002@libero.it <infernet2002@libero.it@20a5eb55-9242-b5b8-544f-9d6c2917c2bf>
2013-07-03 12:40:05 +0000

    no

--- F1Project/src/parser.adb
+++ F1Project/src/parser.adb
@@ -30,5 +30,29 @@ package body parser is
       return First_Ref;
    end readCircuit;
 
+   --- funzione che legge le macchine
+   function readCars (Filename : in String) return arrayOfCars is
+      File       : Ada.Text_IO.File_Type;
+      Line_Count : Natural := 0;
+      Cars_Array : arrayOfCars;
+   begin
+      Ada.Text_IO.Open (File => File,
+                        Mode => Ada.Text_IO.In_File,
+                        Name => Filename);
+      while not Ada.Text_IO.End_Of_File (File) loop
+         declare
+            Line : String := Ada.Text_IO.Get_Line (File);
+         begin
+            Line_Count := Line_Count + 1;
+            Ada.Text_IO.Put_Line (Natural'Image (Line_Count) & ": " & Line);
+            --- in ogni riga ci sono i dati di una macchina
+            --- crea l'array con i dati di ogni macchina
+
+         end;
+      end loop;
+      Ada.Text_IO.Close (File);
+      return Cars_Array;
+   end readCars;
+
 
 end parser;
