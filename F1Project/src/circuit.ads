e65423500800e490f83d87e11a737a5d6c04284d
mick89.mac@gmail.com <mick89.mac@gmail.com@20a5eb55-9242-b5b8-544f-9d6c2917c2bf>
2013-07-03 13:23:15 +0000

    aggiunto il link al parser

--- F1Project/src/circuit.ads
+++ F1Project/src/circuit.ads
@@ -9,11 +9,15 @@ with car_p;
 use car_p;
 with event_bkt;
 use event_bkt;
+with referee_p;
+use referee_p;
 
 
 package Circuit is
 
    event_buffer : Event_Bucket_Access := new Event_Bucket(10);
+
+   task bootstrap;
 
    task weather_forecast;
 
