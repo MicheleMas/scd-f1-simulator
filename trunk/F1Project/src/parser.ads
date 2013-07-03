cbd6be4391bb5b3c112b4501d5523bdd25f386ee
infernet2002@libero.it <infernet2002@libero.it@20a5eb55-9242-b5b8-544f-9d6c2917c2bf>
2013-07-03 12:40:05 +0000

    no

--- trunk/F1Project/src/parser.ads
+++ trunk/F1Project/src/parser.ads
@@ -1,10 +1,15 @@
 with custom_types;
 use custom_types;
+with car_p;
+use car_p;
 with referee_p;
 use referee_p;
 
 package parser is
 
    function readCircuit (Filename : in String) return Referee_Access;
+
+   type arrayOfCars is array(1 .. custom_types.car_number) of Car_Status_Access;
+   function readCars (Filename : in String) return arrayOfCars;
 
 end parser;
