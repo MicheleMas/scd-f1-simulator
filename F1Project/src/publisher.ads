70965174b3cffcdcb081e6b769f3bd9b2bf32874
mick89.mac@gmail.com <mick89.mac@gmail.com@20a5eb55-9242-b5b8-544f-9d6c2917c2bf>
2013-08-14 08:16:39 +0000

    aggiunta una prima versione del protocollo in publisher.ads

--- F1Project/src/publisher.ads
+++ F1Project/src/publisher.ads
@@ -5,6 +5,24 @@ use custom_types;
 
 package Publisher is
 
+   -- Eventi:
+   -- > Fine Segmento
+   --   type = ES | car = (Pos)ID | seg = (Pos)Seg_ID | vel = (Int)speed
+   --   beh = (Pos)behaviour | tire_s = (Int) tire_status
+   --   tire_t = (bool)rain_tire
+   -- > Rientro ai box
+   --   type = EB
+   -- > Uscita dai box
+   --   type = LB | tire_t = (bool)rain_tire
+   -- > Fine Giro
+   --   type = EL | sarebbe bello avere anche il tempo
+   -- > Una macchina conclude la gara
+   --   type = CE | car = (Pos)ID | tempo totale
+   -- > La gara è finita
+   --   type = ER
+   -- > Una macchina si ritira
+   --   type = CR | car = (Pos)ID
+
    task type Event_Handler (event_buffer : Event_Bucket_Access;
                             race_stat : race_status_Access);
 
