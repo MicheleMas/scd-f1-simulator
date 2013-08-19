8e86588825becc7d6ab47fdbd6064db8b9bdc55d
mick89.mac@gmail.com <mick89.mac@gmail.com@20a5eb55-9242-b5b8-544f-9d6c2917c2bf>
2013-08-19 12:56:43 +0000

    rimosso il setup del broker

--- trunk/F1Project/src/broker_race_status.adb
+++ trunk/F1Project/src/broker_race_status.adb
@@ -2,6 +2,19 @@
 
 package body broker_race_status is
 
+   protected body enter_segment is
+
+      function get_time return Integer is
+      begin
+         return time;
+      end get_time;
+      function get_segment return Integer is
+      begin
+         return segment;
+      end get_segment;
+
+   end enter_segment;
+
    protected body race_status is
 
       procedure set_weather(new_weather : in boolean) is
@@ -12,14 +25,14 @@ package body broker_race_status is
       begin
          current_weather := weather;
       end get_weather;
-      function get_laps return Natural is
-      begin
-         return laps;
-      end get_laps;
-      function get_car_number return Natural is
-      begin
-         return car_number;
-      end get_car_number;
+      --function get_laps return Natural is
+      --begin
+      --   return laps;
+      --end get_laps;
+      --function get_car_number return Natural is
+      --begin
+      --   return car_number;
+      --end get_car_number;
 
    end race_status;
 
