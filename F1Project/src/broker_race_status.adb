dce7b44df3b5cd3b52042b99886402608ac6aa57
mick89.mac@gmail.com <mick89.mac@gmail.com@20a5eb55-9242-b5b8-544f-9d6c2917c2bf>
2013-08-20 13:08:59 +0000

    modificato il set ed il get dello snapshot

--- F1Project/src/broker_race_status.adb
+++ F1Project/src/broker_race_status.adb
@@ -19,6 +19,35 @@ package body broker_race_status is
 
    end enter_segment;
 
+   protected body incident_event is
+      function get_time return Integer is
+      begin
+         return time;
+      end get_time;
+      function get_segment return Integer is
+      begin
+         return segment;
+      end get_segment;
+      function is_damaged return boolean is
+      begin
+         return damaged;
+      end is_damaged;
+      function car_retired return boolean is
+      begin
+         return retired;
+      end car_retired;
+
+   end incident_event;
+
+   protected body box_event is
+
+      function get_time return Integer is
+      begin
+         return time;
+      end get_time;
+
+   end box_event;
+
    protected body race_status is
 
       procedure set_weather(new_weather : in boolean) is
@@ -42,39 +71,27 @@ package body broker_race_status is
 
    protected body car_snapshot is
 
-      procedure get_segment(seg : out Integer) is
+      procedure get_data(seg : out Integer;
+                         prog : out Float;
+                         ret : out boolean;
+                         over : out boolean) is
       begin
          seg := segment;
-      end get_segment;
-      procedure get_progress(prog : out Float) is
-      begin
          prog := progress;
-      end get_progress;
-      procedure is_retired(ret : out boolean) is
-      begin
          ret := retired;
-      end is_retired;
-      procedure is_over(over : out boolean) is
-      begin
          over := race_completed;
-      end is_over;
+      end get_data;
 
-      procedure set_segment(seg : in Integer) is
+      procedure set_data(seg : in Integer;
+                         prog : in Float;
+                         ret : in boolean;
+                         over : in boolean) is
       begin
          segment := seg;
-      end set_segment;
-      procedure set_progress(prog : in Float) is
-      begin
          progress := prog;
-      end set_progress;
-      procedure car_retired is
-      begin
-         retired := true;
-      end car_retired;
-      procedure set_over is
-      begin
-         race_completed := true;
-      end set_over;
+         retired := ret;
+         race_completed := over;
+      end set_data;
 
    end car_snapshot;
 
