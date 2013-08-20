60fcb664ff993f6369cde3a2c08141fd399f42b9
mick89.mac@gmail.com <mick89.mac@gmail.com@20a5eb55-9242-b5b8-544f-9d6c2917c2bf>
2013-08-20 15:05:47 +0000

    aggiunto il publisher, anche se per ora non pubblica nulla

---
+++ trunk/F1Project/src/broker_publisher.adb
@@ -0,0 +1,62 @@
+
+
+package body broker_publisher is
+
+   protected body condition is
+      procedure set_up (cars : Integer;
+                       laps : Integer) is
+      begin
+         cars_number := cars;
+         laps_number := laps;
+         set_up_completed := true;
+      end set_up;
+      procedure stop is
+      begin
+         race_over := true;
+      end stop;
+      procedure insert_snapshot(snapshot : in snapshot_array_Access) is
+         snapshot_copy : snapshot_array_Access := new snapshot_array;
+      begin
+         snapshot_copy.all := snapshot.all;
+         bucket_size := bucket_size + 1;
+         bucket.Append(snapshot_copy);
+      end insert_snapshot;
+      entry get_snapshot(snapshot : out snapshot_array_Access) when bucket_size > 0 is
+      begin
+         snapshot := bucket.First_Element;
+         bucket.Delete_First;
+         bucket_size := bucket_size - 1;
+      end get_snapshot;
+      procedure is_bucket_empty(state : out boolean) is
+      begin
+         if(bucket_size = 0)
+         then
+            state := true;
+         else
+            state := false;
+         end if;
+      end is_bucket_empty;
+      procedure set_rain(rain : in boolean) is
+      begin
+         is_raining := rain;
+      end set_rain;
+      procedure get_rain(rain : out boolean) is
+      begin
+         rain := is_raining;
+      end get_rain;
+   end condition;
+
+   task body updater is
+
+   begin
+
+      while(not race_over)
+      loop
+         -- leggi lo stato e convertilo
+         null;
+         delay 0.5;
+      end loop;
+
+   end updater;
+
+end broker_publisher;
