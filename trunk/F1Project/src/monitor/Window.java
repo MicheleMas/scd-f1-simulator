56424be2d7516ea0595d7867a5039418205065cc
mick89.mac@gmail.com <mick89.mac@gmail.com@20a5eb55-9242-b5b8-544f-9d6c2917c2bf>
2013-08-23 12:35:20 +0000

    aggiunta la classe window.java

---
+++ trunk/F1Project/src/monitor/Window.java
@@ -0,0 +1,53 @@
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.*;
+
+public class Window implements Runnable {
+
+	static JFrame frame;
+	static Communicator updater;
+	static boolean stop = false;
+	static int carNumber;
+
+	public Window (Communicator updater, JFrame frame) {
+		this.frame = frame;
+		this.updater = updater;
+		this.carNumber = updater.getCarNumber();
+	}
+
+	public static void stop() {
+		stop = true;
+		updater.stop();
+	}
+
+	public void run() {
+
+		JPanel panel = (JPanel) frame.getContentPane();
+		JLabel label = new JLabel();
+		panel.add(label);
+		label.setText("prova");
+		frame.setLocationRelativeTo(null);
+		frame.pack();
+		frame.setVisible(true);
+
+		Detail det;
+		String text = "";
+
+		while(!stop) {
+			try {
+				text = "";
+				for (int i=0; i<carNumber; i++) {
+					det = updater.raceUpdate(i);
+					if(det != null) {
+						text += "" + i + det.getLap() + " " + det.getSeg() + " " + det.getProg() + "\n";
+					}
+				}
+				label.setText(text);
+				frame.pack();
+				Thread.currentThread().sleep(1000);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
