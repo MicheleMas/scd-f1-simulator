c5c7d460a1121cd4dd239b3237c8ba7670c8adc7
mick89.mac@gmail.com <mick89.mac@gmail.com@20a5eb55-9242-b5b8-544f-9d6c2917c2bf>
2013-08-29 08:07:35 +0000

    La finestra controller è completa, l'override non funziona...

--- trunk/F1Project/src/monitor/ControllerWindow.java
+++ trunk/F1Project/src/monitor/ControllerWindow.java
@@ -14,6 +14,9 @@ public class ControllerWindow extends JPanel implements Runnable {
 	static int HEIGHT = 400;
 	static int driverSelected = 0;
 	static boolean driverSet = false;
+	static JButton boxButton;
+	static JButton behButton;
+	static JFormattedTextField behInput;
 
 	public ControllerWindow(ControllerCommunicator connection, JFrame frame, String[] names, String[] colors) {
 		this.frame = frame;
@@ -31,6 +34,30 @@ public class ControllerWindow extends JPanel implements Runnable {
 		System.out.println("Selezionato pilota "+(driv+1));
 		if (!driverSet) {
 			driverSet = true;
+			boxButton.setEnabled(true);
+			behButton.setEnabled(true);
+		}
+	}
+
+	public void setBoxSchedule() {
+		connection.overrideBoxEntrance(driverSelected);
+	}
+
+	public void setBehaviour() {
+		String reading = behInput.getText();
+		try {
+			int newBehaviour = Integer.parseInt(reading);
+			if (newBehaviour < 1) {
+				newBehaviour = 1;
+				behInput.setText(""+newBehaviour);
+			}
+			if (newBehaviour > 10) {
+				newBehaviour = 10;
+				behInput.setText(""+newBehaviour);
+			}
+			connection.overrideBehaviour(driverSelected, newBehaviour);
+		} catch (NumberFormatException e) {
+			System.out.println("behaviour: not a number");
 		}
 	}
 
@@ -45,18 +72,20 @@ public class ControllerWindow extends JPanel implements Runnable {
 		driversList.setBounds(50, 40, 200, 30);
 		//JLabel infoLabel = new JLabel("<html>Speed = 200<br>AVG Speed = 200<br>Tire Status = 10000<br>Tyre type = rain<br>Behaviour = 10</html>");
 		JLabel infoLabel = new JLabel("");
-		infoLabel.setBounds(30, 80, 240, 130);
+		infoLabel.setBounds(60, 80, 240, 130);
 		JLabel box = new JLabel("Schedule box entrance:");
 		box.setBounds(20, 222, 260, 20);
-		JButton boxButton = new JButton("Schedule Box");
+		boxButton = new JButton("Schedule Box");
 		boxButton.setBounds(60, 250, 180, 20);
+		boxButton.setEnabled(false);
 		JLabel behLabel = new JLabel("Set driver behaviour (1-10):");
 		behLabel.setBounds(20, 280, 260, 20);
-		JFormattedTextField behInput = new JFormattedTextField();
+		behInput = new JFormattedTextField();
 		behInput.setColumns(2);
 		behInput.setBounds(60, 310, 40, 20);
-		JButton behButton = new JButton("Set");
+		behButton = new JButton("Set");
 		behButton.setBounds(160, 310, 80, 20);
+		behButton.setEnabled(false);
 
 		driversList.addActionListener(new ActionListener()
 		{
@@ -75,12 +104,12 @@ public class ControllerWindow extends JPanel implements Runnable {
 		});
 		boxButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// TODO
+				setBoxSchedule();
 			}
 		});
 		behButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// TODO
+				setBehaviour();
 			}
 		});
 		panel.add(title);
@@ -102,10 +131,16 @@ public class ControllerWindow extends JPanel implements Runnable {
 					det = connection.getDetails(driverSelected);
 					text = "<html><table><tr><td><b>Speed</b></td><td>"+det.getSpeed()+"</td></tr>";
 					text += "<tr><td><b>AVG Speed</b></td><td>"+det.getAvgSpeed()+"</td></tr>";
-					text += "<tr><td><b>Tire Status</b></td><td>"+det.getTireStatus()+"</td></tr>";
+					if (det.getTireStatus() == -1)
+						text += "<tr><td><b>Tire Status</b></td><td>"+"BOX"+"</td></tr>";
+					else
+						text += "<tr><td><b>Tire Status</b></td><td>"+det.getTireStatus()+"</td></tr>";
 					text += "<tr><td><b>Tire Type</b></td><td>"+det.getRainTire()+"</td></tr>";
-					text += "<tr><td><b>Behaviour</b></td><td>"+det.getBehaviour()+"</td></tr>";
-					text += "<tr><td><b>Box Schedule</b></td><td>"+" TODO "+"</td></tr>";
+					if (det.getBehaviour() == -1) 
+						text += "<tr><td><b>Behaviour</b></td><td>"+"BOX"+"</td></tr>";
+					else
+						text += "<tr><td><b>Behaviour</b></td><td>"+det.getBehaviour()+"/10</td></tr>";
+					text += "<tr><td><b>Box Schedule</b></td><td>"+det.getRBox()+"</td></tr>";
 					text += "</table></html>";
 					infoLabel.setText(text);
 				}
